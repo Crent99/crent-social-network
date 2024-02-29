ad806e7ffb679a0a41d15a03809908e8da963576
Colin Rent <colinrent@Colins-MacBook-Pro.local>
2024-02-29 18:30:19 -0500

    added comments to controller code

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,13 +1,13 @@
 const { User, Thought } = require("../models");
 
 module.exports = {
-  
+  // Gets all thoughts
   getThought(req, res) {
     Thought.find({})
       .then((thought) => res.json(thought))
       .catch((err) => res.status(500).json(err));
   },
-  
+  // Gets a single thought by its _id
   getSingleThought(req, res) {
     Thought.findOne({ _id: req.params.thoughtId })
       .select("-__v")
@@ -18,7 +18,7 @@ module.exports = {
       )
       .catch((err) => res.status(500).json(err));
   },
-  
+  // Creates thoughts and adds them to the associated user's thoughts array
   createThought(req, res) {
     Thought.create(req.body)
       .then(({ _id }) => {
@@ -35,7 +35,7 @@ module.exports = {
       )
       .catch((err) => res.status(500).json(err));
   },
-  
+  // Updates a thought by its _id
   updateThought(req, res) {
     Thought.findOneAndUpdate(
       { _id: req.params.thoughtId },
@@ -49,7 +49,7 @@ module.exports = {
       )
       .catch((err) => res.status(500).json(err));
   },
-  
+  // Deletes a thought by its _id
   deleteThought(req, res) {
     Thought.findOneAndDelete({ _id: req.params.thoughtId })
       .then((thought) =>
@@ -68,7 +68,7 @@ module.exports = {
       )
       .catch((err) => res.status(500).json(err));
   },
- 
+  // Adds a reaction to a thought
   createReaction(req, res) {
     Thought.findOneAndUpdate(
       { _id: req.params.thoughtId },
@@ -82,7 +82,7 @@ module.exports = {
       )
       .catch((err) => res.status(500).json(err));
   },
- 
+  // Deletes a reaction from a thought by its _id
   deleteReaction(req, res) {
     Thought.findOneAndUpdate(
       { _id: req.params.thoughtId },
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,13 @@
 const { User, Thought } = require("../models");
 
 module.exports = {
-  
+  // Gets all users
   getUser(req, res) {
     User.find({})
       .then((user) => res.json(user))
       .catch((err) => res.status(500).json(err));
   },
-  
+  // Gets a single user by its _id
   getSingleUser(req, res) {
     User.findOne({ _id: req.params.userId })
       .populate("thoughts")
@@ -20,7 +20,7 @@ module.exports = {
       )
       .catch((err) => res.status(500).json(err));
   },
-  
+  // Creates a user
   createUser(req, res) {
     User.create(req.body)
       .then((user) => res.json(user))
@@ -29,7 +29,7 @@ module.exports = {
         return res.status(500).json(err);
       });
   },
-  
+  // Updates a user by its _id
   updateUser(req, res) {
     User.findOneAndUpdate(
       { _id: req.params.userId },
@@ -43,7 +43,7 @@ module.exports = {
       )
       .catch((err) => res.status(500).json(err));
   },
-  
+  // Deletes a user by its _id
   deleteUser(req, res) {
     User.findOneAndDelete({ _id: req.params.userId })
       .then((user) =>
@@ -54,7 +54,7 @@ module.exports = {
       .then(() => res.json({ message: "User and Thought deleted!" }))
       .catch((err) => res.status(500).json(err));
   },
-  
+  // Adds a friend to a user's friend list
   addFriend(req, res) {
     User.findOneAndUpdate(
       { _id: req.params.userId },
@@ -68,7 +68,7 @@ module.exports = {
       )
       .catch((err) => res.status(500).json(err));
   },
-  
+  // Deletes a friend from a user's friend list
   deleteFriend(req, res) {
     User.findOneAndUpdate(
       { _id: req.params.userId },
