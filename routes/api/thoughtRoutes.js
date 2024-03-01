38964d3291199e7dea4f3646a1fd5dd5ec1b760c
Colin Rent <colinrent@Colins-MacBook-Pro.local>
2024-02-29 20:49:46 -0500

    testing code in routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -10,12 +10,26 @@ const {
     deleteReaction
 } = require('../../controllers/thoughtController');
 
-router.route('/').get(getThought).post(createThought);
+// Set up GET all and POST at /api/thoughts
+router
+    .route('/')
+    .get(getThought)
+    .post(createThought);
 
-router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+// Set up GET one, PUT, and DELETE at /api/thoughts/:id
+router
+    .route('/:id')
+    .get(getSingleThought)
+    .put(updateThought)
+    .delete(deleteThought);
 
-router.route('/:thoughtId/reactions').post(createReaction);
+// Set up POST and DELETE at /api/thoughts/:thoughtId/reactions
+router
+    .route('/:thoughtId/reactions')
+    .post(createReaction);
 
-router.route('/:thoughtId/reactions/:reactionsId').delete(deleteReaction);
+router
+    .route('/:thoughtId/reactions/:reactionId')
+    .delete(deleteReaction);
 
 module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -10,10 +10,23 @@ const {
     deleteFriend
 } = require('../../controllers/userController');
 
-router.route('/').get(getUser).post(createUser);
+// Set up GET all and POST at /api/users
+router
+    .route('/')
+    .get(getUser)
+    .post(createUser);
 
-router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+// Set up GET one, PUT, and DELETE at /api/users/:id
+router
+    .route('/:id')
+    .get(getSingleUser)
+    .put(updateUser)
+    .delete(deleteUser);
 
-router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
+// Set up POST and DELETE at /api/users/:userId/friends/:friendId
+router
+    .route('/:userId/friends/:friendId')
+    .post(addFriend)
+    .delete(deleteFriend);
 
 module.exports = router;
