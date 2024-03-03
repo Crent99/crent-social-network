eee8b188f0092d030f05279cd3a39c2809bd0d62
Colin Rent <colinrent@Colins-MacBook-Pro.local>
2024-03-03 02:11:50 -0500

    testing code in config file

--- config/connection.js
+++ config/connection.js
@@ -2,6 +2,9 @@ const { connect, connection } = require('mongoose');
 
 const connectionString = 'mongodb://localhost:27017/socialDB';
 
-connect(connectionString);
+connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 module.exports = connection;
